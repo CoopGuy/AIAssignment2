474cbce2d7202a0057b20d59dfd4227f0be9ab6b
CoopGuy <michaelmazzella12@gmail.com>
2023-10-05 16:41:45 -0400

    Bugfix and add search functions

--- Searches.c
+++ Searches.c
@@ -17,16 +17,22 @@ struct city
     char* name;
     uint16_t straight_distance;
     bool visited;
-    Connection connections[5];
+    Connection* connections[5];
 };
 
-void addConnection(City* src_city, uint32_t dist, City* dest_city)
+void addConnection(City* src_city, City* dest_city, uint32_t dist)
 {
-    Connection* connections = src_city->connections;
-    while(connections)
+    Connection** connections = src_city->connections;
+    while(*connections)
         connections++;
-    connections->distance = dist;
-    connections->connected_city = dest_city;
+    *connections = (Connection*)calloc(1, sizeof(Connection));
+    if(*connections == NULL)
+    {
+        printf("could not malloc connection for %s to %s", src_city->name, dest_city->name);
+        exit(0);
+    }
+    (*connections)->distance = dist;
+    (*connections)->connected_city = dest_city;
 }
 
 City* createCity(char* name, uint16_t dist)
@@ -49,6 +55,41 @@ City* getCity(char* name, City* city_list[], uint32_t list_len)
         if(strcmp(name, city_list[i]->name) == 0)
             return city_list[i];
     }
+    printf("Could not find city %s\n", name);
+    exit(1);
+}
+
+City* breadthFirst(City* start, City* end, uint32_t max_path_len)
+{
+    City** path = calloc(max_path_len, sizeof(City*));
+    if(path == NULL)
+    {
+        perror("Could not alloc space for path in breadth first");
+        exit(0);
+    }
+    path[0] = start;
+}
+
+City* depthFirst(City* start, City* end, uint32_t max_path_len)
+{
+    City** path = calloc(max_path_len, sizeof(City*));
+    if(path == NULL)
+    {
+        perror("Could not alloc space for path in depth first");
+        exit(0);
+    }
+    path[0] = start;
+}
+
+City* AStar(City* start, City* end, uint32_t max_path_len)
+{
+    City** path = calloc(max_path_len, sizeof(City*));
+    if(path == NULL)
+    {
+        perror("Could not alloc space for path in astar");
+        exit(0);
+    }
+    path[0] = start;
 }
 
 int main()
@@ -77,32 +118,37 @@ int main()
         createCity("Zerind", 374)
     };
 
-    #define getCity(a) getCity(a, cities, citiesLen)
-    #define addConnection(a, b, c) addConnection(a, b, c);addConnection(b, a, c);
+    #pragma region /* Setting up connections */
+    #define getCityFromList(a) getCity(a, cities, citiesLen)
+    #define addConnection2Way(a, b, c) addConnection(a, b, c);addConnection(b, a, c);
+    
+    addConnection2Way(getCityFromList("Arad"),      getCityFromList("Zerind"), 75);
+    addConnection2Way(getCityFromList("Arad"),      getCityFromList("Sibiu"), 140);
+    addConnection2Way(getCityFromList("Arad"),      getCityFromList("Timisoara"), 118);
+    addConnection2Way(getCityFromList("Zerind"),   getCityFromList("Oradea"), 71);
+    addConnection2Way(getCityFromList("Oradea"),    getCityFromList("Sibiu"), 151);
+    addConnection2Way(getCityFromList("Timisoara"), getCityFromList("Lugoj"), 111);
+    addConnection2Way(getCityFromList("Lugoj"),     getCityFromList("Mehadia"), 70);
+    addConnection2Way(getCityFromList("Mehadia"),   getCityFromList("Drobeta"), 75);
+    addConnection2Way(getCityFromList("Drobeta"),   getCityFromList("Craiova"), 120);
+    addConnection2Way(getCityFromList("Craiova"),   getCityFromList("Rimnicu Vilcea"), 146);
+    addConnection2Way(getCityFromList("Craiova"),   getCityFromList("Pitesti"), 138);
+    addConnection2Way(getCityFromList("Sibiu"),     getCityFromList("Fagaras"), 99);
+    addConnection2Way(getCityFromList("Sibiu"),     getCityFromList("Rimnicu Vilcea"), 80);
+    addConnection2Way(getCityFromList("Pitesti"),   getCityFromList("Rimnicu Vilcea"), 97);
+    addConnection2Way(getCityFromList("Fagaras"),    getCityFromList("Bucharest"), 211);
+    addConnection2Way(getCityFromList("Pitesti"),   getCityFromList("Bucharest"), 101);
+    addConnection2Way(getCityFromList("Giurgiu"),   getCityFromList("Bucharest"), 90);
+    addConnection2Way(getCityFromList("Bucharest"), getCityFromList("Urziceni"), 85);
+    addConnection2Way(getCityFromList("Urziceni"),  getCityFromList("Hirsova"), 98);
+    addConnection2Way(getCityFromList("Urziceni"),  getCityFromList("Vaslui"), 142);
+    addConnection2Way(getCityFromList("Hirsova"),   getCityFromList("Eforie"), 86);
+    addConnection2Way(getCityFromList("Vaslui"),    getCityFromList("Iasi"), 92);
+    addConnection2Way(getCityFromList("Iasi"),      getCityFromList("Neamt"), 87);
 
-    addConnection(getCity("Arad"),      getCity("Zerind"), 75);
-    addConnection(getCity("Arad"),      getCity("Sibiu"), 140);
-    addConnection(getCity("Arad"),      getCity("Timisoara"), 118);
-    addConnection(getCity("Zerinda"),   getCity("Oradea"), 71);
-    addConnection(getCity("Oradea"),    getCity("Sibiu"), 151);
-    addConnection(getCity("Timisoara"), getCity("Lugoj"), 111);
-    addConnection(getCity("Lugoj"),     getCity("Mehadia"), 70);
-    addConnection(getCity("Mehadia"),   getCity("Drobeta"), 75);
-    addConnection(getCity("Drobeta"),   getCity("Craiova"), 120);
-    addConnection(getCity("Craiova"),   getCity("Rimnicu Vilcea"), 146);
-    addConnection(getCity("Craiova"),   getCity("Pitesti"), 138);
-    addConnection(getCity("Sibiu"),     getCity("Fagaras"), 99);
-    addConnection(getCity("Sibiu"),     getCity("Rimnicu Vilcea"), 80);
-    addConnection(getCity("Pitesti"),   getCity("Rimnicu Vilcea"), 97);
-    addConnection(getCity("Fagras"),    getCity("Bucharest"), 211);
-    addConnection(getCity("Pitesti"),   getCity("Bucharest"), 101);
-    addConnection(getCity("Giurgiu"),   getCity("Bucharest"), 90);
-    addConnection(getCity("Bucharest"), getCity("Urziceni"), 85);
-    addConnection(getCity("Urziceni"),  getCity("Hirsova"), 98);
-    addConnection(getCity("Urziceni"),  getCity("Vaslui"), 142);
-    addConnection(getCity("Hirsova"),   getCity("Eforie"), 86);
-    addConnection(getCity("Vaslui"),    getCity("Iasi"), 92);
-    addConnection(getCity("Iasi"),      getCity("Neamt"), 87);
+    #undef getCityFromList
+    #undef addConnection2Way
+    #pragma endregion 
 
     for(int i = 0; i < citiesLen; i++)
     {
